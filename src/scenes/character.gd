560948d60b95eb9b22fd3b9fec8dabb243fb4a4c
JulianRC1 <juldarecar2003@gmail.com>
2025-11-18 18:02:42 -0500

    Movimiento del personaje y rotación de la cámara sobre el eje del personaje, amplicación de la base

--- src/scenes/character.gd
+++ src/scenes/character.gd
@@ -3,9 +3,26 @@ extends CharacterBody3D
 
 const SPEED = 5.0
 const JUMP_VELOCITY = 4.5
+var sens := 0.05
+
+@onready var cam = $Pivot
+
+func _ready() -> void:
+	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 
 
 func _physics_process(delta: float) -> void:
+	
+	movement(delta)
+	move_and_slide()
+
+func _input(event: InputEvent) -> void:
+	if event is InputEventMouseMotion:
+		rotate_y(deg_to_rad(-event.relative.x * sens))
+		cam.rotate_x(deg_to_rad(-event.relative.y * sens))
+		cam.rotation.x = clamp(cam.rotation.x, deg_to_rad(-90), deg_to_rad(45))
+
+func movement(delta:float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
@@ -16,7 +33,7 @@ func _physics_process(delta: float) -> void:
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var input_dir := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	var input_dir := Input.get_vector("a", "d", "w", "s")
 	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
 		velocity.x = direction.x * SPEED
@@ -24,5 +41,3 @@ func _physics_process(delta: float) -> void:
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		velocity.z = move_toward(velocity.z, 0, SPEED)
-
-	move_and_slide()
