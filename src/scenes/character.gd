b126430c6401d9608f6c0cd2698903945a130d12
JulianRC1 <juldarecar2003@gmail.com>
2025-11-18 19:54:15 -0500

    Introducción del asset del personaje y sus animaciones

--- src/scenes/character.gd
+++ src/scenes/character.gd
@@ -4,8 +4,10 @@ extends CharacterBody3D
 const SPEED = 5.0
 const JUMP_VELOCITY = 4.5
 var sens := 0.05
+var respawnPosition = Vector3(0,4,0)
 
 @onready var cam = $Pivot
+@onready var animationPlayer = $AnimationPlayer
 
 func _ready() -> void:
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
@@ -14,6 +16,7 @@ func _ready() -> void:
 func _physics_process(delta: float) -> void:
 	
 	movement(delta)
+	respawn()
 	move_and_slide()
 
 func _input(event: InputEvent) -> void:
@@ -21,6 +24,10 @@ func _input(event: InputEvent) -> void:
 		rotate_y(deg_to_rad(-event.relative.x * sens))
 		cam.rotate_x(deg_to_rad(-event.relative.y * sens))
 		cam.rotation.x = clamp(cam.rotation.x, deg_to_rad(-90), deg_to_rad(45))
+
+func respawn():
+	if position.y < -10:
+		position = respawnPosition
 
 func movement(delta:float) -> void:
 	# Add the gravity.
@@ -30,6 +37,7 @@ func movement(delta:float) -> void:
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+		animationPlayer.play("Jump")
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
@@ -41,3 +49,16 @@ func movement(delta:float) -> void:
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		velocity.z = move_toward(velocity.z, 0, SPEED)
+		
+	animations(direction)
+
+func animations(direction):
+	if is_on_floor():
+		if direction == Vector3.ZERO:
+			animationPlayer.play("Idle")
+		else:
+			animationPlayer.play("Run")
+	else:
+		if velocity.y < 0:
+			animationPlayer.play("Jump")
+	
